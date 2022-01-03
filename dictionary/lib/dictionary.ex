bfb6c44720692cbd3215f8371540492fdeb8dcaf
Richard Case <richard@weave.works>
2022-01-03 16:30:17 +0000

    Dictionary refactor
    Signed-off-by: Richard Case <richard@weave.works>

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,12 +1,11 @@
 defmodule Dictionary do
+  alias Dictionary.Impl.WordList
 
-  @word_list "assets/words.txt"
-    |>File.read!()
-    |>String.split(~r/\n/, trim: true)
+  @opaque t :: WordList.t
 
+  @spec start() :: t
+  defdelegate start, to: WordList, as: :word_list
 
-  def random_word do
-    @word_list
-    |> Enum.random()
-  end
+  @spec random_word(t) :: String.t
+  defdelegate random_word(word_list), to: WordList
 end
